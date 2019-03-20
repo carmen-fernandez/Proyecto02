f21660bb2eece85ce98016d4fcb67bfdb6f73885
carmen_fernandez <cmfernandez102@gmail.com>
2019-03-20 15:53:01 +0100

    Logger

--- Agenda-Proyecto02/src/main/java/spring/services/Services_Impl.java
+++ Agenda-Proyecto02/src/main/java/spring/services/Services_Impl.java
@@ -1,5 +1,10 @@
 package spring.services;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Services_Impl {
+	
+	private static final Logger logger = LogManager.getLogger("Mensaje");
 
 }
